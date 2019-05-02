4fefe505ac862c81490e86ed65f4b26640487833
Filipe Falcão <filipefalcaosantos@gmail.com>
2019-05-02 00:53:38 -0300

    Formatting.

--- src/main/java/lexical/LexicalTable.java
+++ src/main/java/lexical/LexicalTable.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+
 class LexicalTable {
 
     static HashMap<String, Tokens> map;
--- src/main/java/utils/Grammar.java
+++ src/main/java/utils/Grammar.java
@@ -5,6 +5,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 
+
 public class Grammar {
 
     private ArrayList<Production> productions = new ArrayList<>();
